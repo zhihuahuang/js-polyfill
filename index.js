77b7ad7b4606d503585b2265b856aba15b85bb7b
huangzhihua <huangzhihua@ihuayue.cn>
2018-09-11 14:22:26 +0800

    0.1.1

---
+++ index.js
@@ -0,0 +1,39 @@
+/**
+ * Array
+ */
+require('Array.from');
+require('Array.isArray');
+require('Array.of');
+require('Array.prototype.copyWithin');
+require('Array.prototype.every');
+require('Array.prototype.fill');
+require('Array.prototype.filter');
+require('Array.prototype.find');
+require('Array.prototype.forEach');
+require('Array.prototype.includes');
+require('Array.prototype.lastIndexOf');
+require('Array.prototype.map');
+require('Array.prototype.reduce');
+require('Array.prototype.reduceRight');
+require('Array.prototype.some');
+
+/**
+ * Number
+ */
+require('Number.isFinite');
+require('Number.isInteger');
+require('Number.isNaN');
+require('Number.isSafeInteger');
+require('Number.MAX_SAFE_INTEGER');
+require('Number.parseFloat');
+require('Number.parseInt');
+
+/**
+ * Object
+ */
+require('Object.assign');
+require('Object.create');
+require('Object.entries');
+require('Object.is');
+require('Object.keys');
+require('Object.values');
